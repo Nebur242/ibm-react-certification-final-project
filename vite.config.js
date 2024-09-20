4f194fdb2f708ce39de1fac875af4eeb214c8d87
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2024-09-20 15:31:30 +0200

    fix: removed dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ibm-react-certification-final-project",
   plugins: [react()],
 });
